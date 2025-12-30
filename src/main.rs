7e0f4421a1a23189c347114107fa3d7c9bfdb2be
Kevin W Matthews <kevinwmatthews@gmail.com>
2025-12-30 10:25:34 -0800

    Initialize External IO driver

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use esp_idf_sys as _;
+use esp_idf_sys::I2C_Init;
+use esp_idf_sys::EXIO_Init;
 
 fn main() {
     println!("Hello, world!");
+    unsafe { I2C_Init() };
+    unsafe { EXIO_Init() };
 }
