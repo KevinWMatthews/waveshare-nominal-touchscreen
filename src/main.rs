13db7bdb124536f3d033f18e4ce6ec44e05b0920
Kevin W Matthews <kevinwmatthews@gmail.com>
2025-12-30 10:25:34 -0800

    Initialize touch driver

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use esp_idf_sys as _;
 use esp_idf_sys::I2C_Init;
 use esp_idf_sys::EXIO_Init;
 use esp_idf_sys::LCD_Init;
+use esp_idf_sys::Touch_Init;
 use std::ptr;
 
 fn main() {
@@ -9,4 +10,5 @@ fn main() {
     unsafe { I2C_Init() };
     unsafe { EXIO_Init() };
     unsafe { LCD_Init(ptr::null_mut()) };
+    unsafe { Touch_Init() };
 }
