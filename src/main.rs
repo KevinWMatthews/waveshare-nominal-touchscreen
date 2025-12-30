6360685e50c145ab77a87ae2580a3b955bb1c47b
Kevin W Matthews <kevinwmatthews@gmail.com>
2025-12-30 10:25:34 -0800

    Initialze and read from touch pad
    No console output yet, but calls do not crash.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,32 @@ use esp_idf_sys as _;
 use esp_idf_sys::I2C_Init;
 use esp_idf_sys::EXIO_Init;
 use esp_idf_sys::LCD_Init;
+use esp_idf_sys::QMI8658_Init;
+use esp_idf_sys::QMI8658_Loop;
 use esp_idf_sys::Touch_Init;
+use esp_idf_sys::vTaskDelay;
 use std::ptr;
 
 fn main() {
     println!("Hello, world!");
     unsafe { I2C_Init() };
+    unsafe { QMI8658_Init() };
     unsafe { EXIO_Init() };
     unsafe { LCD_Init(ptr::null_mut()) };
     unsafe { Touch_Init() };
+    loop {
+        unsafe { QMI8658_Loop() };
+        unsafe { vTaskDelay(ms_to_ticks(100)) };
+    }
+}
+
+/// A Rust definition of the FreeRTOS macro pdMS_TO_TICKS.
+///
+/// The original definition is:
+/// ```c
+/// #define pdMS_TO_TICKS( xTimeInMs )    ( ( TickType_t ) ( ( ( uint64_t ) ( xTimeInMs ) * ( uint64_t ) configTICK_RATE_HZ ) / ( uint64_t ) 1000U ) )
+/// ```
+fn ms_to_ticks(time_in_ms: u32) -> u32 {
+    // TODO Be wary of overflow
+    (time_in_ms * esp_idf_sys::configTICK_RATE_HZ) / 1000
 }
