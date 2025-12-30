80bfde840f125523c3afc1b56c00b49dfaeee989
Kevin W Matthews <kevinwmatthews@gmail.com>
2025-12-30 10:25:34 -0800

    Initialize LCD

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use esp_idf_sys as _;
 use esp_idf_sys::I2C_Init;
 use esp_idf_sys::EXIO_Init;
+use esp_idf_sys::LCD_Init;
+use std::ptr;
 
 fn main() {
     println!("Hello, world!");
     unsafe { I2C_Init() };
     unsafe { EXIO_Init() };
+    unsafe { LCD_Init(ptr::null_mut()) };
 }
