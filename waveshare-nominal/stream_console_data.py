c10117e7f324176586dd5643c90d247952c2ad0f
Kevin W Matthews <kevinwmatthews@gmail.com>
2026-01-04 15:48:12 -0700

    Graph touchscreen, acceleration, and gyroscope data in Nominal

--- waveshare-nominal/stream_console_data.py
+++ waveshare-nominal/stream_console_data.py
@@ -1,6 +1,6 @@
 import re
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Optional, List
 
 import connect_python
 import serial
@@ -9,12 +9,46 @@ from connect_python.ts import ConnectTimestamp, Nanoseconds
 logger = connect_python.get_logger(__name__)
 
 
-class TouchscreenReading:
-    NOMINAL_LOG_TAG = "NOMINAL"
+class ReadingParser:
+    @staticmethod
+    def parse(data: str) -> Optional[TouchscreenReading | AccelerometerReading | GyroscopeReading]:
+        match = TouchscreenReading.parse(data)
+        if match:
+            return match
 
+        match = AccelerometerReading.parse(data)
+        if match:
+            return match
+
+        match = GyroscopeReading.parse(data)
+        if match:
+            return match
+
+        return None
+
+
+class TouchscreenReading:
+    STREAM_ID: str = 'touch'
+    channel_names: List[str] = ['x', 'y', 'z']
     timestamp: ConnectTimestamp
     x: int
     y: int
+
+    # Sample data (with color disabled):
+    # b'I (18786) NOMINAL TOUCH: X=259 Y=362\r\n'
+    # Regex key:
+    # `^`: start anchor
+    # `$`: end anchor
+    # `?`: match 0 or more
+    # `+`: match 1 or more
+    # `[]`: a character set
+    # `()`: A capture group
+    # `\(` and `\)`: escaped parenthesis (not a capture group)
+    # `?P<name>`: A named capture group
+    # `\d`: one or more digits
+    # `\s`: a space
+    REGEX = re.compile(
+        r"^(?P<level>I)\s\((?P<timestamp>\d+)\)\s(?P<module>NOMINAL TOUCH):\sX=(?P<x>\d+)\sY=(?P<y>\d+)$")
 
     def __init__(self, time_since_boot: Nanoseconds, x: int, y: int) -> None:
         self.timestamp = time_since_boot
@@ -24,37 +58,146 @@ class TouchscreenReading:
     def __str__(self) -> str:
         return f'TouchReading: ({self.timestamp}) X={self.x}, Y={self.y}'
 
-    @staticmethod
     def parse(data: str) -> Optional[TouchscreenReading]:
-        # Sample data (with color disabled):
-        # b'I (18786) NOMINAL: X=259 Y=362\r\n'
-        # Regex key::
-        # `^`: start anchor
-        # `$`: end anchor
-        # `()`: A capture group
-        # `\(` and `\)`: escaped parenthesis (not a capture group)
-        # `?P<name>`: A named capture group
-        # `\d+`: one or more digits
-        # `\s`: a space
-        pattern = r"^(?P<level>I)\s\((?P<timestamp>\d+)\)\s(?P<module>NOMINAL):\sX=(?P<x>\d+)\sY=(?P<y>\d+)$"
-        match = re.search(pattern, data)
+        match = TouchscreenReading.REGEX.search(data)
         if match is None:
             return None
 
-        module = match.group('module')
-        if module != TouchscreenReading.NOMINAL_LOG_TAG:
-            return None
-
+        # Log data is expected to be at the Info level
         log_level = match.group('level')
         if log_level != 'I':
             return None
 
+        # Espressif log timestamps are in 'ms since boot'
         time_since_boot_ms = match.group('timestamp')
-        time_since_boot_ns = int(time_since_boot_ms) * 1_000_000  # TODO Will this always match?
+        time_since_boot_ns = int(time_since_boot_ms) * 1_000_000
 
+        # The regex should ensure that these match groups contain integer data
         x = int(match.group('x'))
         y = int(match.group('y'))
         return TouchscreenReading(time_since_boot_ns, x, y)
+
+    def channel_names(self) -> List[str]:
+        return ['x', 'y']
+
+    def channel_values(self) -> List[float]:
+        return [self.x, self.y]
+
+
+class AccelerometerReading:
+    STREAM_ID: str = "accel"
+    timestamp: ConnectTimestamp
+    x: float
+    y: float
+    z: float
+    # Sample data (with color disabled):
+    # b'I (191252) NOMINAL ACCEL: X = 0.032836914, Y = 0.13195801, Z = 0.13195801\r\n'
+    # Regex key:
+    # `^`: start anchor
+    # `$`: end anchor
+    # `?`: match 0 or more
+    # `+`: match 1 or more
+    # `[]`: a character set
+    # `()`: A capture group
+    # `\(` and `\)`: escaped parenthesis (not a capture group)
+    # `?P<name>`: A named capture group
+    # `\d`: one or more digits
+    # `\s`: a space
+    REGEX = re.compile(
+        r"^(?P<level>I)\s\((?P<timestamp>\d+)\)\s(?P<module>NOMINAL ACCEL):\sX=(?P<x>[-]?\d.\d+)\sY=(?P<y>[-]?\d.\d+)\sZ=(?P<z>[-]?\d.\d+)$")
+
+    def __init__(self, time_since_boot: Nanoseconds, x: float, y: float, z: float) -> None:
+        self.timestamp = time_since_boot
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self) -> str:
+        return f'AccelReading: ({self.timestamp}) X={self.x}, Y={self.y}, Z={self.z}'
+
+    def parse(data: str) -> Optional[AccelerometerReading]:
+        match = AccelerometerReading.REGEX.search(data)
+        if match is None:
+            return None
+
+        # Log data is expected to be at the Info level
+        log_level = match.group('level')
+        if log_level != 'I':
+            return None
+
+        # Espressif log timestamps are in 'ms since boot'
+        time_since_boot_ms = match.group('timestamp')
+        time_since_boot_ns = int(time_since_boot_ms) * 1_000_000
+
+        # The regex should ensure that these match groups contain float data
+        x = float(match.group('x'))
+        y = float(match.group('y'))
+        z = float(match.group('z'))
+        return AccelerometerReading(time_since_boot_ns, x, y, z)
+
+    def channel_names(self) -> List[str]:
+        return ['x', 'y', 'z']
+
+    def channel_values(self) -> List[float]:
+        return [self.x, self.y, self.z]
+
+
+class GyroscopeReading:
+    STREAM_ID: str = "gyro"
+    timestamp: ConnectTimestamp
+    x: float
+    y: float
+    z: float
+    # Sample data (with color disabled):
+    # b'I (191252) NOMINAL GYRO: X = 2.8339844, Y = 1.5917969, Z = 1.5917969\r\n'
+    # Regex key:
+    # `^`: start anchor
+    # `$`: end anchor
+    # `?`: match 0 or more
+    # `+`: match 1 or more
+    # `[]`: a character set
+    # `()`: A capture group
+    # `\(` and `\)`: escaped parenthesis (not a capture group)
+    # `?P<name>`: A named capture group
+    # `\d`: one or more digits
+    # `\s`: a space
+    REGEX = re.compile(
+        r"^(?P<level>I)\s\((?P<timestamp>\d+)\)\s(?P<module>NOMINAL GYRO):\sX=(?P<x>[-]?\d.\d+)\sY=(?P<y>[-]?\d.\d+)\sZ=(?P<z>[-]?\d.\d+)$")
+
+    def __init__(self, time_since_boot: Nanoseconds, x: float, y: float, z: float) -> None:
+        self.timestamp = time_since_boot
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self) -> str:
+        return f'GyroReading: ({self.timestamp}) X={self.x}, Y={self.y}, Z={self.z}'
+
+    def parse(data: str) -> Optional[GyroscopeReading]:
+        match = GyroscopeReading.REGEX.search(data)
+        if match is None:
+            return None
+
+        # Log data is expected to be at the Info level
+        log_level = match.group('level')
+        if log_level != 'I':
+            return None
+
+        # Espressif log timestamps are in 'ms since boot'
+        time_since_boot_ms = match.group('timestamp')
+        time_since_boot_ns = int(time_since_boot_ms) * 1_000_000
+
+        # The regex should ensure that these match groups contain float data
+        x = float(match.group('x'))
+        y = float(match.group('y'))
+        z = float(match.group('z'))
+        return GyroscopeReading(time_since_boot_ns, x, y, z)
+
+    def channel_names(self) -> List[str]:
+        return ['x', 'y', 'z']
+
+    def channel_values(self) -> List[float]:
+        return [self.x, self.y, self.z]
 
 
 @connect_python.main
@@ -78,19 +221,25 @@ def stream_console_data(client: connect_python.Client):
                     logger.debug('Ignoring non-unicode data')
                     continue
 
-                logger.debug(line)
-                reading: Optional[TouchscreenReading] = TouchscreenReading.parse(line)
+                if client.get_value('log_console'):
+                    logger.info(line)
+                reading = ReadingParser.parse(line)
                 if not reading:
-                    logger.debug('Ignored reading')
+                    logger.debug('Ignored line of console data')
                     continue
-                logger.info(reading)
 
-                client.stream(
-                    "touch", reading.timestamp, reading.x, name="x_coord", start_time=start_time
-                )
-                client.stream(
-                    "touch", reading.timestamp, reading.y, name="y_coord", start_time=start_time
-                )
+                client.stream(reading.STREAM_ID, reading.timestamp, start_time=start_time,
+                              names=reading.channel_names(), values=reading.channel_values())
+
+                if type(reading) == TouchscreenReading:
+                    if client.get_value('log_touch'):
+                        logger.info(reading)
+                elif type(reading) == AccelerometerReading:
+                    if client.get_value('log_accel'):
+                        logger.info(reading)
+                elif type(reading) == GyroscopeReading:
+                    if client.get_value('log_gyro'):
+                        logger.info(reading)
     except serial.serialutil.SerialException as e:
         # Inform the user if the serial connection can't be formed or fails
         logger.error(f'Serial device error: {e}')
